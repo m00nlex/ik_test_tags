737e562c9bf979f2b6ae27b7e41ac8dc0f6b1a2c
m00nlex <m00n-lex@yandex.ru>
2022-12-26 00:11:54 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,11 @@
+// document.querySelector("#header").addEventListener("mouseenter", showText)
+// document.querySelector("#footer").addEventListener("mouseenter", showText)
+// // function showText(dot){
+//     dot.target.style.color = "red"
+//     setTimeout(hideText, 2000, dot)
+// }
+//
+// function hideText(dot){
+//     dot.target.style.color = "white"
+// }
+
